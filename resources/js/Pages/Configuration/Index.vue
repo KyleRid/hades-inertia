6716525b1fc818443df43b26294e4d3b0b46ad9b
Vladimir Agapov <kyleridrus@gmail.com>
2021-02-02 13:09:34 +0300

    fix configuration

--- resources/js/Pages/Configuration/Index.vue
+++ resources/js/Pages/Configuration/Index.vue
@@ -7,37 +7,37 @@
             <form @submit.prevent="save()"  v-if="!isLoading">
                 <div class="form-group">
                     <label for="sitename">Site name</label>
-                    <input id="sitename" type="text" placeholder="Site Name" v-model="form.siteName">
+                    <input id="sitename" type="text" placeholder="Site Name" v-model="form.siteName.value">
                 </div>
 
                 <div class="form-group">
                     <label for="sitedescription">Site description</label>
-                    <input id="sitedescription" type="text" placeholder="Site Description" v-model="form.siteDescription">
+                    <input id="sitedescription" type="text" placeholder="Site Description" v-model="form.siteDescription.value">
                 </div>
 
                  <div class="form-group">
                     <label for="titleTag">Title Tag</label>
-                    <input id="titleTag" type="text" placeholder="Site Title" v-model="form.siteTitle">
+                    <input id="titleTag" type="text" placeholder="Site Title" v-model="form.siteTitle.value">
                 </div>
 
                 <div class="form-group">
                     <label for="adminEmail">Admin Email</label>
-                    <input id="adminEmail" type="text" placeholder="Admin Email" v-model="form.adminEmail">
+                    <input id="adminEmail" type="text" placeholder="Admin Email" v-model="form.adminEmail.value">
                 </div>
 
                 <div class="form-group">
                     <label for="allowlogin">Allow Login</label>
-                    <input id="allowlogin" type="checkbox" v-model="form.allowLogin">
+                    <input id="allowlogin" type="checkbox" v-model="form.allowLogin.value">
                 </div>
 
                 <div class="form-group">
                     <label for="allowregistration">Allow Registration</label>
-                    <input id="allowregistration" type="checkbox" v-model="form.allowRegistration">
+                    <input id="allowregistration" type="checkbox" v-model="form.allowRegistration.value">
                 </div>
 
                 <div class="form-group">
                     <label for="maintenance">Maintenance Mode</label>
-                    <input id="maintenance" type="checkbox" v-model="form.maintenanceMode">
+                    <input id="maintenance" type="checkbox" v-model="form.maintenanceMode.value">
                 </div>
                 <button class="btn btn-success" type="submit">Save</button>
             </form>
@@ -86,20 +86,31 @@
             this.configuration.forEach((item) => {
                 this.form[item.option_name] = {
                     id: item.option_id,
-                    value: item.option_value,
+                    value: this.getValue(item.option_name, item.option_value),
                     name: item.option_name,
                 }
             });
-            console.log('form', this.form);
             console.log(this.siteData);
             this.isLoading = false;
             console.log('configuration', this.configuration);
+            console.log('form', this.form);
         },
         methods: {
             async save() {
                 this.$inertia.patch('/configuration', this.form).then(() => {
                     console.log('saved');
                 });
+            },
+            getValue(name, value) {
+                switch (name) {
+                    case 'maintenanceMode':
+                    case 'allowRegistration':
+                    case 'allowLogin':
+                        return !!+value;
+                        break;
+                    default:
+                        return value;
+                }
             }
         }
     }
